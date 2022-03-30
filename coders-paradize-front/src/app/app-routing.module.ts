a9494828c8a704436f66339751a885fd9502ad2e
MahomedM <mahomedmachel@gmail.com>
2022-03-30 15:27:13 +0200

    La Nav-barre

--- coders-paradize-front/src/app/app-routing.module.ts
+++ coders-paradize-front/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EvenementsComponent } from './evenements/evenements.component';
+import { GoodiesComponent } from './goodies/goodies.component';
+import { MustReadComponent } from './must-read/must-read.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MustReadComponent },
+  { path: 'goodies', component: GoodiesComponent },
+  { path: 'evenements', component: EvenementsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
