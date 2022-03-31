b3dcf60033c56ba1bb25fa1fde7beb2d8625e244
jerkoLegion <jerome.potie@gmail.com>
2022-03-31 11:55:57 +0200

    Add logo and the navbar is fonctionnal

--- coders-paradize-front/src/app/app-routing.module.ts
+++ coders-paradize-front/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EvenementsComponent } from './evenements/evenements.component';
 import { GoodiesComponent } from './goodies/goodies.component';
+import { HomeComponent } from './home/home.component';
 import { MustReadComponent } from './must-read/must-read.component';
 
 const routes: Routes = [
-  { path: '', component: MustReadComponent },
+  { path: '', component: HomeComponent },
+  { path: 'must-read', component: MustReadComponent },
   { path: 'goodies', component: GoodiesComponent },
   { path: 'evenements', component: EvenementsComponent },
 ];
--- coders-paradize-front/src/app/app.module.ts
+++ coders-paradize-front/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MustReadComponent } from './must-read/must-read.component';
 import { GoodiesComponent } from './goodies/goodies.component';
 import { EvenementsComponent } from './evenements/evenements.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, MustReadComponent, GoodiesComponent, EvenementsComponent],
+  declarations: [AppComponent, NavBarComponent, MustReadComponent, GoodiesComponent, EvenementsComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
